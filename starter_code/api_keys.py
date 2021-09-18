ce397601a254d8749f8eb999026990de31362331
Momin <kmch43@hotmail.com>
2021-09-18 11:09:23 +1000

    Submitting Gmap analysis

---
+++ starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "30b07a8a7dcf189197c5d62313fa7b2c"#"414f4f869dca308ba1b16223231a3636"
+
+# Google API Key
+g_key = "AIzaSyBKkLJKkxENOvMqx_GUlK-4K3vVqUGV32g"
